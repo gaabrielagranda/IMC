52fa629ff8664f2b56d297a4348867b503291d80
Gabriela Granda <gabrielagrandatarrio@gmail.com>
2024-01-18 11:24:56 +0100

    First version

--- src/IMC.java
+++ src/IMC.java
@@ -5,11 +5,12 @@ Objetivo: Calcular el IMC de una persona.
 Este programa calcula el Indice de Masa Corporal.
  */
 
+import java.util.Locale;
 import java.util.Scanner;
 public class IMC {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.us);
-        //Variables
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        /* Variables */
         String name = "";
         int age = 0;
         double weight = 0.0;
@@ -20,14 +21,14 @@ public class IMC {
         System.out.println("Person form");
         System.out.println("====================");
         System.out.println();
-        System.out.println("Nombre: ");
-        nombre = scanner.nextLine();
-        System.out.println("Edad: ");
-        edad = Integer.parseInt(scanner.nextLine());
-        System.out.println("Peso: ");
-        peso = scanner.nextDouble();
-        System.out.println("Altura: ");
-        altura = scanner.nextDouble();
+        System.out.println("Name: ");
+        name = scanner.nextLine();
+        System.out.println("Age: ");
+        age = Integer.parseInt(scanner.nextLine());
+        System.out.println("Weight: ");
+        weight = scanner.nextDouble();
+        System.out.println("Height: ");
+        height = scanner.nextDouble();
         //End of form
 
         /*process */
